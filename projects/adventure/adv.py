c2bd6c66b196bfc23f9074f98106bc0d3a745c4b
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-11 23:08:42 -0700

    adventure passes test in 996 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,8 +28,58 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+stack = [player.current_room.id]
+map_size = 500
+rooms_visited = 1
+traveled_map = {player.current_room.id: {}}
 
-
+while rooms_visited < map_size:
+    #add base room to the stack
+    #add one child to the stack - do this until you hit a dead end (add direction to traversal_path each time)
+    #remove from stack until you get to a node with an open path
+    if 'n' in player.current_room.get_exits() and player.current_room.n_to.id not in traveled_map:
+        traveled_map[player.current_room.id]['n'] = player.current_room.n_to.id
+        traveled_map[player.current_room.n_to.id] = {'s': player.current_room.id}
+        traversal_path.append('n')
+        stack.append(player.current_room.n_to.id)
+        player.travel('n')
+        rooms_visited += 1
+    elif 's' in player.current_room.get_exits() and player.current_room.s_to.id not in traveled_map:
+        traveled_map[player.current_room.id]['s'] = player.current_room.s_to.id
+        traveled_map[player.current_room.s_to.id] = {'n': player.current_room.id}
+        traversal_path.append('s')
+        stack.append(player.current_room.s_to.id)
+        player.travel('s')
+        rooms_visited += 1
+    elif 'e' in player.current_room.get_exits() and player.current_room.e_to.id not in traveled_map:
+        traveled_map[player.current_room.id]['e'] = player.current_room.e_to.id
+        traveled_map[player.current_room.e_to.id] = {'w': player.current_room.id}
+        traversal_path.append('e')
+        stack.append(player.current_room.e_to.id)
+        player.travel('e')
+        rooms_visited += 1
+    elif 'w' in player.current_room.get_exits() and player.current_room.w_to.id not in traveled_map:
+        traveled_map[player.current_room.id]['w'] = player.current_room.w_to.id
+        traveled_map[player.current_room.w_to.id]= {'e': player.current_room.id}
+        traversal_path.append('w')
+        stack.append(player.current_room.w_to.id)
+        player.travel('w')
+        rooms_visited += 1
+    #dead end case
+    else:
+        stack.pop()
+        if 'n' in player.current_room.get_exits() and traveled_map[player.current_room.id]['n'] == stack[-1]:
+            traversal_path.append('n')
+            player.travel('n')
+        elif 's' in player.current_room.get_exits() and traveled_map[player.current_room.id]['s'] == stack[-1]:
+            traversal_path.append('s')
+            player.travel('s')
+        elif 'e' in player.current_room.get_exits() and traveled_map[player.current_room.id]['e'] == stack[-1]:
+            traversal_path.append('e')
+            player.travel('e')
+        elif 'w' in player.current_room.get_exits() and traveled_map[player.current_room.id]['w'] == stack[-1]:
+            traversal_path.append('w')
+            player.travel('w')
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +101,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
